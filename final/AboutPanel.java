f080c133ca27d68ae1b1173839520ea04192050d
Jyoti Suri <38820874+JyotiSuri@users.noreply.github.com>
2019-12-05 01:17:20 -0800

    Update AboutPanel.java

--- final/AboutPanel.java
+++ final/AboutPanel.java
@@ -11,22 +11,6 @@ public class AboutPanel extends JPanel {
 	 * Constructs an instance of the about panel
 	 */
 	public AboutPanel() {
-//		JLabel a = new JLabel();
-//		a.setText("Akshat Bansal 			Github: https://github.com/akshat2610");
-//		JLabel ap = new JLabel();
-//		ap.setIcon(new ImageIcon("./images/aboutPics/akshatAbout.JPEG"));
-//		JLabel j = new JLabel();
-//		j.setText("Jyoti Suri 				Github: https://github.com/JyotiSuri");
-//		JLabel jp = new JLabel();
-//		jp.setIcon(new ImageIcon("./images/aboutPics/jyotiAbout.JPEG"));
-//		JLabel s = new JLabel();
-//		s.setText("Shruti Panchal 			Github: https://github.com/shrutipanchal7699");
-//		JLabel sp = new JLabel();
-//		sp.setIcon(new ImageIcon("./images/aboutPics/shrutiAbout.JPG"));
-//		JLabel t = new JLabel();
-//		t.setText("Tirth Patel 				Github: https://github.com/tirthnpatel");
-//		JLabel tp = new JLabel();
-//		tp.setIcon(new ImageIcon("./images/aboutPics/tirthAbout.JPG"));
 
 		aboutAPanel q = new aboutAPanel();
 		aboutJPanel w = new aboutJPanel();
